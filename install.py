69952b0feb540f771783cdcca85d4f94618ad8f8
Symphanny <symphannysec@protonmail.com>
2020-05-19 22:40:11 -0400

    Create install.py

---
+++ install.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("pip3 install requests")
+os.system("pip3 install selenium")
+os.system("pip3 install bs4")
+print("Installation is complete!!")
+print("Please run python3 instagramselenium.py")
